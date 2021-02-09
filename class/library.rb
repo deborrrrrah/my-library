97f78585729675db9b67ba8a1f10dbdbc02fb9bd
deborrrrrah <dajtpbl@gmail.com>
2021-02-10 02:32:01 +0700

    [Debo] Library#valid? return true when shelf_size 3, row_size 2, column_size 2

---
+++ class/library.rb
@@ -0,0 +1,13 @@
+class Library
+  def initialize(params)
+    @books = Hash.new
+    @shelf_size = params[:shelf_size]
+    @row_size = params[:row_size]
+    @column_size = params[:column_size]
+    @available_position = '010101'
+  end
+
+  def valid?
+    true
+  end
+end
---
+++ spec/library_spec.rb
@@ -0,0 +1,16 @@
+require '../class/library.rb'
+
+RSpec.describe 'Library' do
+  describe '#valid?' do
+    it 'return true when shelf_size 3, row_size 2, column_size 2' do
+      params = {
+        shelf_size: 3,
+        row_size: 2,
+        column_size: 2
+      }
+      library = Library.new(params)
+      result = library.valid?
+      expect(result).to eq(true)
+    end
+  end
+end
