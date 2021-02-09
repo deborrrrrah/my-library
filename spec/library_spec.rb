082f103dd59a9e035fbac80d7e6fdbf5d0e26134
deborrrrrah <dajtpbl@gmail.com>
2021-02-10 02:41:22 +0700

    [Debo] Library#valid? return false when shelf_size 3, row_size 2, column_size 100

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -73,5 +73,16 @@ RSpec.describe 'Library' do
       result = library.valid?
       expect(result).to eq(false)
     end
+
+    it 'return false when shelf_size 3, row_size 2, column_size 100' do
+      params = {
+        shelf_size: 3,
+        row_size: 2,
+        column_size: 100
+      }
+      library = Library.new(params)
+      result = library.valid?
+      expect(result).to eq(false)
+    end
   end
 end
