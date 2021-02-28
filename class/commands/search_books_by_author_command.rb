13270f234f76c6f9fa832a20ba3accf705168669
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 04:31:26 +0700

    [Debo] Add SearchBooksByAuthorCommand to stub search_book_by_author

---
+++ class/commands/search_books_by_author_command.rb
@@ -0,0 +1,8 @@
+require '../class/library.rb'
+require_relative 'command'
+
+class SearchBooksByAuthorCommand < Command
+  def execute(args)
+    Library.instance.search_book_by_author(args[0])
+  end
+end
---
+++ spec/commands/search_books_by_author_command_spec.rb
@@ -0,0 +1,11 @@
+require '../class/commands/search_books_by_author_command.rb'
+require '../class/library.rb'
+
+RSpec.describe 'SearchBooksByAuthorCommand' do
+  it 'stub Library#search_book_by_author' do
+    command = SearchBooksByAuthorCommand.new
+    args = ['Kent Beck']
+    expect_any_instance_of(Library).to receive(:search_book_by_author)
+    command.execute(args)
+  end
+end
