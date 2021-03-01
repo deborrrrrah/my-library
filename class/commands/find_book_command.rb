3bd9f7afa43ebbf1347e1cc744587b24428727db
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 21:38:19 +0700

    [Debo] Add FindBookCommand#args_valid? return true when only one args

--- class/commands/find_book_command.rb
+++ class/commands/find_book_command.rb
@@ -3,6 +3,10 @@ require_relative 'command'
 require './class/book_address.rb'
 
 class FindBookCommand < Command
+  def args_valid?(args)
+    true
+  end
+
   def execute(args)
     Library.instance.find_book(args[0])
   end
--- spec/commands/find_book_command_spec.rb
+++ spec/commands/find_book_command_spec.rb
@@ -2,6 +2,14 @@ require '../class/commands/find_book_command.rb'
 require '../class/library.rb'
 
 RSpec.describe 'FindBookCommand' do
+  describe '#args_valid?' do
+    it 'return true when only one args' do
+      command = FindBookCommand.new
+      args = ['9780807281918']
+      expect(command.args_valid?(args)).to eq(true)
+    end
+  end
+  
   describe '#execute' do
     it 'stub Library#find_book' do
       command = FindBookCommand.new
