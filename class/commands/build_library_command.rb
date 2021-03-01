5c96e90a91bbe4013984d511800dbf70ec9cf469
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 16:25:45 +0700

    [Debo] BuildLibraryCommand#execute fix stub by adding params parsed

--- class/commands/build_library_command.rb
+++ class/commands/build_library_command.rb
@@ -3,11 +3,11 @@ require_relative 'command'
 
 class BuildLibraryCommand < Command
   def execute(args)
-    params = Hash.new({
+    params = {
       shelf_size: args[0],
       row_size: args[1],
       column_size: args[2]
-    })
+    }
     Library.instance.reset_size(params)
   end
 end
--- spec/commands/build_library_command_spec.rb
+++ spec/commands/build_library_command_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe 'BuildLibraryCommand' do
   it 'stub Library#reset_size' do
     command = BuildLibraryCommand.new
     args = [2, 1, 3]
-    expect_any_instance_of(Library).to receive(:reset_size)
+    params = {
+      shelf_size: 2,
+      row_size: 1,
+      column_size: 3
+    }
+    expect_any_instance_of(Library).to receive(:reset_size).with(params)
     command.execute(args)
   end
 end
