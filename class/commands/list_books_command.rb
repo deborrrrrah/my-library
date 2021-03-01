046c7d95db5a0e28fa8ca70811b7b4d4aa13fef2
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 19:01:52 +0700

    [Debo] Add ListBooksCommand#args_valid? return true on any condition

--- class/commands/list_books_command.rb
+++ class/commands/list_books_command.rb
@@ -2,6 +2,10 @@ require './class/library.rb'
 require_relative 'command'
 
 class ListBooksCommand < Command
+  def args_valid?(args)
+    true
+  end
+
   def execute(args = [])
     Library.instance.list_books
   end
--- spec/commands/list_books_command_spec.rb
+++ spec/commands/list_books_command_spec.rb
@@ -2,9 +2,19 @@ require '../class/commands/list_books_command.rb'
 require '../class/library.rb'
 
 RSpec.describe 'ListBooksCommand' do
-  it 'stub Library#list_books' do
-    command = ListBooksCommand.new
-    expect_any_instance_of(Library).to receive(:list_books)
-    command.execute
+  describe '#args_valid?' do
+    it 'always return true' do
+      command = ListBooksCommand.new
+      args = []
+      expect(command.args_valid?(args)).to eq(true)
+    end 
+  end
+  
+  describe '#execute' do
+    it 'stub Library#list_books' do
+      command = ListBooksCommand.new
+      expect_any_instance_of(Library).to receive(:list_books)
+      command.execute
+    end
   end
 end
