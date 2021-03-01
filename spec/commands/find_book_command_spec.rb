0563fd39f5cde00ebdeae8927cde30f1ce246232
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 16:29:11 +0700

    [Debo] Fix FindBookCommand#execute spec to pass parameter in the stub

--- spec/commands/find_book_command_spec.rb
+++ spec/commands/find_book_command_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'FindBookCommand' do
   it 'stub Library#find_book' do
     command = FindBookCommand.new
     args = ['9780807281918']
-    expect_any_instance_of(Library).to receive(:find_book)
+    expect_any_instance_of(Library).to receive(:find_book).with(args[0])
     command.execute(args)
   end
 end
