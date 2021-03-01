93fe3320f1eb88509932300235afc8a75b9ff3ec
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 19:08:55 +0700

    [Debo] Add SearchBooksByAuthorCommand#args_valid? return true when argument consist of one argument

--- class/commands/search_books_by_author_command.rb
+++ class/commands/search_books_by_author_command.rb
@@ -2,6 +2,10 @@ require './class/library.rb'
 require_relative 'command'
 
 class SearchBooksByAuthorCommand < Command
+  def args_valid?(args)
+    true
+  end
+
   def execute(args)
     Library.instance.search_book_by_author(args[0])
   end
--- spec/commands/search_books_by_author_command_spec.rb
+++ spec/commands/search_books_by_author_command_spec.rb
@@ -2,10 +2,19 @@ require '../class/commands/search_books_by_author_command.rb'
 require '../class/library.rb'
 
 RSpec.describe 'SearchBooksByAuthorCommand' do
-  it 'stub Library#search_book_by_author' do
-    command = SearchBooksByAuthorCommand.new
-    args = ['Kent Beck']
-    expect_any_instance_of(Library).to receive(:search_book_by_author).with(args[0])
-    command.execute(args)
+  describe '#args_valid?' do
+    it 'return true when argument consist of one arguments' do
+      command = SearchBooksByAuthorCommand.new
+      args = ['Kent Beck']
+      expect(command.args_valid?(args)).to eq(true)
+    end
+  end
+  describe '#execute' do
+    it 'stub Library#search_book_by_author' do
+      command = SearchBooksByAuthorCommand.new
+      args = ['Kent Beck']
+      expect_any_instance_of(Library).to receive(:search_book_by_author).with(args[0])
+      command.execute(args)
+    end
   end
 end
