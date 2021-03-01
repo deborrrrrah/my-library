df17ec7f108035313e832999c5eeec92e485e602
deborrrrrah <dajtpbl@gmail.com>
2021-03-01 19:11:59 +0700

    [Debo] Add SearchBooksByTitleCommand#args_valid? return true when argument only consist of one element

--- spec/commands/search_books_by_title_command_spec.rb
+++ spec/commands/search_books_by_title_command_spec.rb
@@ -2,10 +2,20 @@ require '../class/commands/search_books_by_title_command.rb'
 require '../class/library.rb'
 
 RSpec.describe 'SearchBooksByTitleCommand' do
-  it 'stub Library#search_book_by_title' do
-    command = SearchBooksByTitleCommand.new
-    args = ['Harry Potter 1']
-    expect_any_instance_of(Library).to receive(:search_book_by_title).with(args[0])
-    command.execute(args)
+  describe '#args_valid?' do
+    it 'return true when argument only consist of one element' do
+      command = SearchBooksByTitleCommand.new
+      args = ['Harry Potter 1']
+      expect(command.args_valid?(args)).to eq(true)
+    end
+  end
+  
+  describe '#execute' do
+    it 'stub Library#search_book_by_title' do
+      command = SearchBooksByTitleCommand.new
+      args = ['Harry Potter 1']
+      expect_any_instance_of(Library).to receive(:search_book_by_title).with(args[0])
+      command.execute(args)
+    end
   end
 end
